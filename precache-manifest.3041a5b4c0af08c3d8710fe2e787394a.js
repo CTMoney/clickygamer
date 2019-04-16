9105f9c3507203b0d008ad5d3db042fb7bd80476
CTMoney <money1062@live.com>
2019-04-16 19:19:29 -0400

    Updates

---
+++ precache-manifest.3041a5b4c0af08c3d8710fe2e787394a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "becc8181d6008486e358",
+    "url": "/clickygamer/static/js/runtime~main.af6e2607.js"
+  },
+  {
+    "revision": "e45b3b74b0c3467f92fb",
+    "url": "/clickygamer/static/js/main.09c9522b.chunk.js"
+  },
+  {
+    "revision": "cacda7babc45aa7707ff",
+    "url": "/clickygamer/static/js/2.8fa5f609.chunk.js"
+  },
+  {
+    "revision": "13acaf6a7798512b146f49ed73932a5d",
+    "url": "/clickygamer/index.html"
+  }
+];
